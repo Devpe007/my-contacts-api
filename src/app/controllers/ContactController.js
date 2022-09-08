4a58f45f65d2aa826249de63697d5aaff53ea532
Devpe007 <anddev007pe@gmail.com>
2022-09-07 22:14:14 -0300

    create store method in ContactController controllers

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -19,6 +19,34 @@ class ContactController {
     return response.json(contact);
   };
 
+  async store(request, response) {
+    const {
+      name,
+      email,
+      phone,
+      category_id,
+    } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    };
+
+    const contactExists = await ContactsRepository.findByEmail(email);
+
+    if (contactExists) {
+      return response.status(400).json({ error: 'This e-mail is alredy been taken' });
+    };
+
+    const contact = await ContactsRepository.create({
+      name,
+      email,
+      phone,
+      category_id,
+    });
+
+    return response.json(contact);
+  };
+
   async delete(request, response) {
     const { id } = request.params;
 
