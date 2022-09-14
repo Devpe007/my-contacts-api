0d0eb6bbb2cc3dc73e040e1f70d2b656d204ae9d
Devpe007 <anddev007pe@gmail.com>
2022-09-14 08:48:16 -0300

    create the errorHandler middleware

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('express-async-errors');
 const express = require('express');
 
 const router = require('./routes');
@@ -7,5 +8,10 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000'));
