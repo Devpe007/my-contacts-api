b0791d03c5fad96b6875989d947477f89e66b134
Devpe007 <anddev007pe@gmail.com>
2022-09-19 08:59:11 -0300

    create the cors middleware

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ const express = require('express');
 
 const router = require('./routes');
 
+const cors = require('./app/middlewares/cors');
+
+const errorHandler = require('./app/middlewares/errorHandler');
+
 const app = express();
 
 app.use(express.json());
 
+app.use(cors);
+
 app.use(router);
 
-app.use((error, request, response, next) => {
-  console.log(error);
-  response.sendStatus(500);
-});
+app.use(errorHandler);
 
 app.listen(3001, () => console.log('Server started at http://localhost:3001'));
